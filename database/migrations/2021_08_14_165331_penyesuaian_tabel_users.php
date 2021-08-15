e8bbaf5ba8e6e9a0f94bc5e0e2b2035d6fe00a7f
blessseeker <khoirkamaludin@gmail.com>
2021-08-15 16:37:22 +0700

    menyesuaikan tabel users dan memmbuat user seeder

---
+++ database/migrations/2021_08_14_165331_penyesuaian_tabel_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PenyesuaianTabelUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->unique();
+            $table->string('customer_id')->unique();
+            $table->string('phone');
+            $table->enum('status', ['ACTIVE', 'INACTIVE']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+            $table->dropColumn('customer_id');
+            $table->dropColumn('phone');
+            $table->dropColumn('status');
+        });
+    }
+}
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $user = new \App\Models\User();
+        $user->username = 'kamaludinkhoir';
+        $user->name = 'Kamaludin Khoir';
+        $user->email = 'khoirkamaludin@gmail.com';
+        $user->password = Hash::make('kamaludin');
+        $user->customer_id = 1;
+        $user->phone = '08112276780';
+        $user->status = 'ACTIVE';
+
+        $user->save();
+
+        $this->command->info('User kamaludinkhoir berhasil ditambahkan');
+    }
+}
